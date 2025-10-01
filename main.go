229f4614da46555591c13a4bf375b49b1e0ed509
default <natnael.debesay@stu.ocsb.ca>
2025-10-01 16:41:04 +0000

    main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+/*
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-01
+ * @fileoverview This program practices with string literals.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	// In programming you often use // in front of text to tell
+	// the computer not to interpret or compile these lines
+	// Programmers call this comments.
+
+	//string literal text
+	fmt.Println("I love Computer Science!!")
+	
+	//string literal text
+	fmt.Println("    I  love Computer Science!!")
+
+	//string literal text
+	fmt.Println("I   love   Computer   Science   100%")
+
+	fmt.Println("\nDone.")
+}
